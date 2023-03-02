c1a5504446b7bd94c5b1327e45a5085fc69ed2f9
codeDevLogan <logandevscode@gmail.com>
2023-03-01 21:54:30 -0700

    This is a finished product Next step is Heroku Deployment.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -43,8 +43,8 @@ module.exports = () => {
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 512],
-            destination: path.join('assets', 'logo'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
           }
         ]
       }),
